e835eae74bcbfd9dcad6c9c219c63649f7327baa
Ariel <arieltanami122@gmail.com>
2025-05-20 15:25:23 +0300

    Remove obsolete AssemblyInfo.cs file as it's unnecessary for .NET Core/5+/8.0 projects.

--- Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// כל ההגדרות האלו מיותרות בפרויקט NET Core/NET 5+ (כולל NET 8.0)
-// יש למחוק את כל ה-assembly attributes מהקובץ הזה, כי הם נוצרים אוטומטית מה-csproj
-//
-// קובץ זה יכול להישאר ריק או להימחק לחלוטין
